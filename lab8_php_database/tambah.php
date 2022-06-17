d7d6095a81dd3aa867c37eb2b4b73bac0416b8e8
hanzabasskiller23 <hannatadjava23@gmail.com>
2022-06-17 15:52:06 +0700

    revamp

--- lab8_php_database/tambah.php
+++ lab8_php_database/tambah.php
@@ -20,10 +20,8 @@ if (isset($_POST['submit']))
             $gambar = 'gambar/' . $filename;;
         }
     }
-    $sql = 'INSERT INTO data_barang (nama, kategori,harga_jual, harga_beli,
-    stok, gambar) ';
-    $sql .= "VALUE ('{$nama}', '{$kategori}','{$harga_jual}',
-    '{$harga_beli}', '{$stok}', '{$gambar}')";
+    $sql = 'INSERT INTO data_barang (nama, kategori,harga_jual, harga_beli, stok, gambar) ';
+    $sql = "VALUE ('{$nama}', '{$kategori}','{$harga_jual}', '{$harga_beli}', '{$stok}', '{$gambar}')";
     $result = mysqli_query($conn, $sql);
     header('location: index.php');
 }
@@ -31,49 +29,48 @@ if (isset($_POST['submit']))
 <!DOCTYPE html>
 <html lang="en">
 <head>
- <meta charset="UTF-8">
- <link href="style.css" rel="stylesheet" type="text/css" />
- <title>Tambah Barang</title>
+    <meta charset="UTF-8">
+    <link href="style.css" rel="stylesheet" type="text/css" />
+    <title>Tambah Barang</title>
 </head>
 <body>
 <div class="container">
- <h1>Tambah Barang</h1>
- <div class="main">
- <form method="post" action="tambah.php"
-enctype="multipart/form-data">
- <div class="input">
- <label>Nama Barang</label>
- <input type="text" name="nama" />
- </div>
- <div class="input">
- <label>Kategori</label>
- <select name="kategori">
- <option value="Komputer">Komputer</option>
- <option value="Elektronik">Elektronik</option>
- <option value="Hand Phone">Hand Phone</option>
- </select>
- </div>
- <div class="input">
- <label>Harga Jual</label>
- <input type="text" name="harga_jual" />
- </div>
- <div class="input">
- <label>Harga Beli</label>
- <input type="text" name="harga_beli" />
- </div>
- <div class="input">
- <label>Stok</label>
- <input type="text" name="stok" />
- </div>
- <div class="input">
- <label>File Gambar</label>
- <input type="file" name="file_gambar" />
- </div>
- <div class="submit">
- <input type="submit" name="submit" value="Simpan" />
- </div>
- </form>
- </div>
- </div>
+    <h1>Tambah Barang</h1>
+    <div class="main">
+        <form method="post" action="tambah.php" enctype="multipart/form-data">
+            <div class="input">
+                <label>Nama Barang</label>
+                <input type="text" name="nama" />
+            </div>
+            <div class="input">
+            <label>Kategori</label>
+            <select name="kategori">
+                <option value="Komputer">Komputer</option>
+                <option value="Elektronik">Elektronik</option>
+                <option value="Hand Phone">Hand Phone</option>
+            </select>
+            </div>
+            <div class="input">
+                <label>Harga Jual</label>
+                <input type="text" name="harga_jual" />
+            </div>
+            <div class="input">
+                <label>Harga Beli</label>
+                <input type="text" name="harga_beli" />
+            </div>
+            <div class="input">
+                <label>Stok</label>
+                <input type="text" name="stok" />
+            </div>
+            <div class="input">
+                <label>File Gambar</label>
+                <input type="file" name="file_gambar" />
+            </div>
+            <div class="submit">
+                <input type="submit" name="submit" value="Simpan" />
+            </div>
+        </form>
+    </div>
+</div>
 </body>
 </html>
--- lab8_php_database/ubah.php
+++ lab8_php_database/ubah.php
@@ -22,10 +22,9 @@ if (isset($_POST['submit']))
         $gambar = 'gambar/' . $filename;;
         }
     }
-    $sql = 'UPDATE data_barang SET ';
+$sql = 'UPDATE data_barang SET ';
     $sql .= "nama = '{$nama}', kategori = '{$kategori}', ";
-    $sql .= "harga_jual = '{$harga_jual}', harga_beli = '{$harga_beli}', stok
-   = '{$stok}' ";
+    $sql .= "harga_jual = '{$harga_jual}', harga_beli = '{$harga_beli}', stok = '{$stok}' ";
     if (!empty($gambar))
     $sql .= ", gambar = '{$gambar}' ";
     $sql .= "WHERE id_barang = '{$id}'";
@@ -53,51 +52,42 @@ if (isset($_POST['submit']))
 <body>
     <div class="container">
     <h1>Ubah Barang</h1>
-    <div class="main">
-    <form method="post" action="ubah.php"
-    enctype="multipart/form-data">
-    <div class="input">
-    <label>Nama Barang</label>
-    <input type="text" name="nama" value="<?php echo
-    $data['nama'];?>" />
+        <div class="main">
+            <form method="post" action="ubah.php" enctype="multipart/form-data">
+                <div class="input">
+                    <label>Nama Barang</label>
+                    <input type="text" name="nama" value="<?php echo $data['nama'];?>" />
+                </div>
+                <div class="input">
+                    <label>Kategori</label>
+                    <select name="kategori">
+                        <option <?php echo is_select ('Komputer', $data['kategori']);?> value="Komputer">Komputer</option>
+                        <option <?php echo is_select ('Komputer', $data['kategori']);?> value="Elektronik">Elektronik</option>
+                        <option <?php echo is_select ('Komputer', $data['kategori']);?> value="Hand Phone">Hand Phone</option>
+                    </select>
+                </div>
+                <div class="input">
+                    <label>Harga Jual</label>
+                    <input type="text" name="harga_jual" value="<?php echo $data['harga_jual'];?>" />
+                </div>
+                <div class="input">
+                    <label>Harga Beli</label>
+                    <input type="text" name="harga_beli" value="<?php echo $data['harga_beli'];?>" />
+                </div>
+                <div class="input">
+                    <label>Stok</label>
+                    <input type="text" name="stok" value="<?php echo $data['stok'];?>" />
+                </div>
+                <div class="input">
+                    <label>File Gambar</label>
+                    <input type="file" name="file_gambar" />
+                </div>
+                <div class="submit">
+                    <input type="hidden" name="id" value="<?php echo $data['id_barang'];?>" />
+                    <input type="submit" name="submit" value="Simpan" />
+                </div>
+            </form>
+        </div>
     </div>
-    <div class="input">
-    <label>Kategori</label>
-    <select name="kategori">
-    <option <?php echo is_select
-('Komputer', $data['kategori']);?> value="Komputer">Komputer</option>
- <option <?php echo is_select
-('Komputer', $data['kategori']);?> value="Elektronik">Elektronik</option>
- <option <?php echo is_select
-('Komputer', $data['kategori']);?> value="Hand Phone">Hand Phone</option>
- </select>
- </div>
- <div class="input">
- <label>Harga Jual</label>
- <input type="text" name="harga_jual" value="<?php echo
-$data['harga_jual'];?>" />
- </div>
- <div class="input">
- <label>Harga Beli</label>
- <input type="text" name="harga_beli" value="<?php echo
-$data['harga_beli'];?>" />
- </div>
- <div class="input">
- <label>Stok</label>
- <input type="text" name="stok" value="<?php echo
-$data['stok'];?>" />
- </div>
- <div class="input">
- <label>File Gambar</label>
- <input type="file" name="file_gambar" />
- </div>
- <div class="submit">
- <input type="hidden" name="id" value="<?php echo
-$data['id_barang'];?>" />
- <input type="submit" name="submit" value="Simpan" />
- </div>
- </form>
- </div>
-</div>
 </body>
 </html>
